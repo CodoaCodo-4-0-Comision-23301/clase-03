06b1d8f85e78858d2167371193ebd9954cf36a2d
Walter Liendo <walter.liendo@gmail.com>
2023-03-06 20:43:41 -0300

    creacion js

---
+++ Proyecto_clase03/js/app.js
@@ -0,0 +1,8 @@
+var a = 232;
+let b = 120;  // arraca desde el ecma6
+let myDOM = document;
+console.log(myDOM);
+
+let myApp = document.getElementById("myApp");
+
+myApp.innerHTML = "Estoy modificando el placeholder"
