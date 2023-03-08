27090882121f0df1a895e9508dc39b7e3241b580
Walter Liendo <walter.liendo@gmail.com>
2023-03-08 19:44:36 -0300

    incorporo prompt al div

--- Proyecto_clase03/js/app.js
+++ Proyecto_clase03/js/app.js
@@ -1,8 +1,8 @@
-var a = 232;
-let b = 120;  // arraca desde el ecma6
-let myDOM = document;
-console.log(myDOM);
+/* Prompt */
+let name = prompt("Cual es tu nombre?")
 
+/* div*/
 let myApp = document.getElementById("myApp");
 
-myApp.innerHTML = "Estoy modificando el placeholder"
+/* reemplazo la info del html */
+myApp.innerHTML = "Hola " + name + "!";
