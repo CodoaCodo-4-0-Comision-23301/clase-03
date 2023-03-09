c85c6ca66ab1c34433b05e681a9bf150a4619e11
Walter Liendo <walter.liendo@gmail.com>
2023-03-08 22:09:21 -0300

    img

--- Proyecto_clase03/js/app.js
+++ Proyecto_clase03/js/app.js
@@ -5,4 +5,4 @@ let name = prompt("Cual es tu nombre?")
 let myApp = document.getElementById("myApp");
 
 /* reemplazo la info del html */
-myApp.innerHTML = "Hola " + name + "!";
+myApp.innerHTML = "Hola " + name + "!, Bienvenido a mi sitio el cual no tuve tiempo de terminarlo, disculpen!!";
